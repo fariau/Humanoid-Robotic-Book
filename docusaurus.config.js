e077dace59d16c4ffbd37b8568deb500eec6c7d7
fariau <usmanfaria88@gmail.com>
2025-12-13 13:07:28 +0500

    chore: Add deployment branch configuration for GitHub Pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'fariau', // Usually your GitHub org/user name.
   projectName: 'Humanoid-Robotic-Book', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // The branch to deploy to
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
